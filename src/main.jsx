c4f99ac7a7ee461e288e4bfebc413616ba990c3f
rafael <rafael.penner465@gmail.com>
2023-11-14 18:54:36 -0300

    various updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
-import { MyContext } from './myContext';
+import { AuthProvider } from './hooks/auth';
 
 import theme from './styles/theme'
 
@@ -12,10 +12,10 @@ import { Routes } from './routes';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <MyContext.Provider value={{ email: 'rafael@email.com' }}>
-        <Routes />
-      </MyContext.Provider >
+      <GlobalStyles /> 
+      <AuthProvider>
+        <Routes />      
+      </AuthProvider>     
     </ThemeProvider>
   </React.StrictMode>,
 );
