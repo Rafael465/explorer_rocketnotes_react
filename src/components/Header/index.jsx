417fd6a9fe4b322a924294faad71d016c3b9d352
rafael <rafael.penner465@gmail.com>
2023-11-15 15:19:58 -0300

    updated signOut

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 
@@ -7,6 +8,12 @@ import { Container, Profile, Logout } from "./styles";
 
 export function Header(){
     const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation("/");
+        signOut();
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -24,7 +31,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
